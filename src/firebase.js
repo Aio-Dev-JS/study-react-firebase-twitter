2a6305b2e2f9014b806c80cc00aed4e286448032
Aio-Dev-JS <AioDev.JS@gmail.com>
2020-09-13 23:03:27 +0900

    Setup

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  // github에 올라가는 것만 방지, RCA에서는 노출 됨, 환경변수는 반드시
+  // REACT_APP으로 시작해야함
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
---
+++ src/routes/Profile.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const Profile = () => <span>Porfile</span>;
+export default Profile;
