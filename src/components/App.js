5e541a77fe3cf8570204e19eb6808f55c84c1d1e
Aio-Dev-JS <AioDev.JS@gmail.com>
2020-09-15 02:02:21 +0900

    Lecture 3.3 Realtime update Tweet with firetore

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import AppRouter from 'components/Router';
-import fbase, { authService } from 'fBase';
-//  jsconfig에서 절대 경로 사용 가능하게 만들어줌
+import { authService } from 'fBase';
 
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -19,8 +20,12 @@ function App() {
 
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'initiallizing'}
-      <footer>&copy; {new Date().getFullYear()} Nwitter </footer>
+      {init ? (
+        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+      ) : (
+        'Initializing...'
+      )}
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
 }
